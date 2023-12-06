96aea602ebfc4b594ef18ca0059d4889719afe7c
jay <wuj20061224@gmail.com>
2023-12-06 08:09:09 +0800

    Use raygui button example

--- src/display.c
+++ src/display.c
@@ -1,4 +1,8 @@
 #include "raylib.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 #include <stdio.h>
 
 #include "display.h"
@@ -7,12 +11,22 @@
 int main() {
 
     InitWindow(WIDTH, HEIGHT, "Display Tower of Hanoi");
-    
+    SetTargetFPS(60);
+
+    int count = 0;
+    char message[10] ;
 
     while (!WindowShouldClose()) {
 
         BeginDrawing();
+            ClearBackground(BLACK);
 
+            if ( GuiButton( (Rectangle){200, 200, 50, 20}, "Click me" ) ) {
+                ++count;
+                sprintf(message, "%d times", count);
+                printf("click\n");
+            }
+            DrawText(message, 200, 260, 20, WHITE) ;
             DrawText("Hello from raylib", 100, 100, 30, BLUE);
 
         EndDrawing();
